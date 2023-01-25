c9ec903bd80d695c55b76d1f11b36b18a4a3be20
PannetonB <pannetonb@gmail.com>
2023-01-25 11:09:13 -0500

    Modif pour pouvoir reprendre le répertoire de données et l'identifiant unique des fichiers utilisés la dernière fois.

--- R/mainRaman_w_Models.R
+++ R/mainRaman_w_Models.R
@@ -100,11 +100,22 @@ mainRaman_w_Models <- function()
   ## Définir plan d'expérience et chemin pour stocker les données ----
   #********************************************************
   Plan <- GetPlanExp()
-  dataSetID <-utils::winDialogString(
-    "Entrer un identifiant pour les noms de fichier de données",
-    as.character(Sys.Date()))
-  dataPath <- utils::choose.dir(default = "",
-        caption = "Choisir un répertoire pour stocker les données.")
+  load('dataPaths_n_names.RData')
+  if (dataPath!=""){
+    dumAnswer <- utils::winDialog(type="yesno",
+                                  message=paste("Voulez-vous stocker les données dans",
+                                                dataPath,'\\..._',dataSetID))
+    
+  }
+  
+  if ((dataPath=="") | (dumAnswer=="NO")){
+    dataSetID <-utils::winDialogString(
+      "Entrer un identifiant pour les noms de fichier de données",
+      as.character(Sys.Date()))
+    dataPath <- utils::choose.dir(default = "",
+                                  caption = "Choisir un répertoire pour stocker les données.")
+    save(dataPath,dataSetID, file="dataPaths_n_names.RData")
+  }
   
   #********************************************************
   ## Permettre modif paramètres d'acquisition à chaque échantillon ----
